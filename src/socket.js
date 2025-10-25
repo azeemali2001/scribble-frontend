d3bb24548b7899a292ae5ae64bf66b1f475ef9a3
Azeem Ali <aliazeem7428@gmail.com>
2025-10-25 17:20:44 +0530

    Change socket connection

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,6 @@
-// src/socket.js
 import { io } from "socket.io-client";
 
-// Use deployed backend URL instead of localhost
-export const socket = io("https://scribble-balckend.onrender.com");
+export const socket = io("https://scribble-balckend.onrender.com", {
+  transports: ["websocket", "polling"], // force transports
+  withCredentials: true
+});
